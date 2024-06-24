d769ed614a61b971bf58ff3c8d742647cbed855f
bananalden <128482606+bananalden@users.noreply.github.com>
2024-06-24 15:45:05 +0800

    created a bootstrap navbar

--- frontend/webpage-frontend/src/App.jsx
+++ frontend/webpage-frontend/src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Login from './login/loginform.jsx'
+import Navbar from './navbar/navbar.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
 
   return (
     <>
-      <Login />
+      <Navbar />
     </>
   )
 }
