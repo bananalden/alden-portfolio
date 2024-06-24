253d0217b61941f3c7fad48378971161fd91b956
bananalden <128482606+bananalden@users.noreply.github.com>
2024-06-24 12:27:09 +0800

    editing login form

--- frontend/webpage-frontend/src/login/loginform.jsx
+++ frontend/webpage-frontend/src/login/loginform.jsx
@@ -1,21 +1,21 @@
 function loginForm (){
     return(
-        <div className="main">
+        <div className="loginbody">
             <h2>Welcome to Alden's Portfolio!</h2>
             <p>To view, please log in!</p>
-        <div className="loginform">
+        
             <form action="" method="post">
             <h3>Log In</h3>
-            <label>Username:
-            <input type="text" name="" id="" />
+            <label className="usernameLabel">Username:
+            <input  className="inputUser" type="text" name="" id="" />
             </label>
-            <label>Password:
+            <label className="passwordLabel">Password:
             <input type="password" name="" id="" />
             </label>
             <button type="submit">Log In</button>
             </form>
         </div>
-    </div>
+
 
     );
 
