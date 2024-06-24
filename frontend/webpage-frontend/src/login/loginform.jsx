f51c5e9e937765b82c91cd7be1eb41e6a580d4cd
bananalden <128482606+bananalden@users.noreply.github.com>
2024-06-24 09:59:15 +0800

    initial commit

---
+++ frontend/webpage-frontend/src/login/loginform.jsx
@@ -0,0 +1,20 @@
+function loginForm (){
+    return(
+        <div>
+            <form action="" method="post">
+            <h1>Log In</h1>
+            <label>Username:</label>
+            <input type="text" name="" id="" />
+            <label>Password:</label>
+            <input type="password" name="" id="" />
+            <button type="submit">Log In</button>
+            </form>
+        </div>
+
+
+    );
+
+}
+
+
+export default loginForm;
