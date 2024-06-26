430026105856551d960b08690be436803c22c1ae
bananalden <128482606+bananalden@users.noreply.github.com>
2024-06-26 09:30:53 +0800

    added another accordion item in about

--- frontend/webpage-frontend/src/routes/about.jsx
+++ frontend/webpage-frontend/src/routes/about.jsx
@@ -30,13 +30,21 @@ function about(){
       <Accordion.Item eventKey="1">
         <Accordion.Header>Hobbies</Accordion.Header>
         <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          <ul>
+              <li>Watching Fireship videos</li>
+              <li>Playing management games</li>
+              <li>Reading Classic Literature</li>
+          </ul>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="2">
+        <Accordion.Header>Likes</Accordion.Header>
+        <Accordion.Body>
+          <ul>
+              <li>Limbus Company</li>
+              <li>Cities Skyline</li>
+              <li>The Metamorphosis by Franz Kafka</li>
+          </ul>
         </Accordion.Body>
       </Accordion.Item>
     </Accordion>
