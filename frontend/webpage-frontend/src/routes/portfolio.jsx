314cc63632d426fbfa8a95f509346714e779495a
bananalden <128482606+bananalden@users.noreply.github.com>
2024-06-28 15:07:32 +0800

    added reactjs section to portfolio route

--- frontend/webpage-frontend/src/routes/portfolio.jsx
+++ frontend/webpage-frontend/src/routes/portfolio.jsx
@@ -10,6 +10,9 @@ import Timein from "../assets/portfolioCard/aldenTimeinsystem.png";
 import LibSystem1 from '../assets/imagesforPortfolio/phpProjects/librarySystem/library1.png'
 import LibSystem2 from '../assets/imagesforPortfolio/phpProjects/librarySystem/library2.png'
 import LibSystem3 from '../assets/imagesforPortfolio/phpProjects/librarySystem/library3.png'
+import TimeIn1 from '../assets/imagesforPortfolio/phpProjects/employeeAttendance/employeeAttendance1.png';
+import TimeIn2 from '../assets/imagesforPortfolio/phpProjects/employeeAttendance/employeeAttendance2.png';
+import TimeIn3 from '../assets/imagesforPortfolio/phpProjects/employeeAttendance/employeeAttendance3.png';
 
 
 
@@ -18,9 +21,25 @@ function portfolio (){
 
         <Container className="myContainer">
             <h1>Portfolio</h1>
-            <Container className="subBack">
+            <Container className="subBack m-3">
                 <h1>PHP Projects</h1>
             <div className="d-md-flex">
+            <PortfolioCard 
+           systemsample = { Timein }
+           title = "Employee Time In System in Colllaboration with MJ"
+           description = "An employee clock-in system created in fulfillment of our requirements during 3rd year 1st Semester"
+           bigDesc = "This was the first system I made with MJ. Completely raw with no libraries to work with, me and MJ worked on everything from the ground up from the UI elements to the backend logic"
+           carImage1 = { TimeIn1 }
+            carImage2 = { TimeIn2 }
+            carImage3 = { TimeIn3 }
+            carCap1 = "The login form of our page"
+            carCap2 = "The main page was designed by MJ"
+            carCap3 = "This is the inventory list of the books available to borrow"
+           
+           
+           /> 
+
+
             <PortfolioCard 
             systemsample = { LibrarySystem }
             title = "Library System in Collaboration with MJ"
@@ -34,6 +53,28 @@ function portfolio (){
             carCap3 = "This is the inventory list of the books available to borrow"
            
            />
+           </div>
+            </Container>
+            <Container className="subBack m-3">
+                <h1>ReactJS Projects</h1>
+            <div className="d-md-flex">
+            <PortfolioCard 
+           systemsample = { Timein }
+           title = "Employee Time In System in Colllaboration with MJ"
+           description = "An employee clock-in system created in fulfillment of our requirements during 3rd year 1st Semester"
+           bigDesc = "This was the first system I made with MJ. Completely raw with no libraries to work with, me and MJ worked on everything from the ground up from the UI elements to the backend logic"
+           carImage1 = { TimeIn1 }
+            carImage2 = { TimeIn2 }
+            carImage3 = { TimeIn3 }
+            carCap1 = "The login form of our page"
+            carCap2 = "The main page was designed by MJ"
+            carCap3 = "This is the inventory list of the books available to borrow"
+           
+           
+           /> 
+
+
+         
            </div>
             </Container>
             
